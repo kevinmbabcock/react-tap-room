b37e5c3e8f66a2103f5c87deee6030fa87b63d5a
Kevin M Babcock <kevinmbabcock@Kevins-MacBook-Air.local>
2018-02-09 10:33:10 -0800

    add list of kegs and render them in Keg component

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Keg(){
+
+function Keg(props){
 
   return (
     <div>
-      <p>Keg</p>
+      <h6><strong>{props.name}</strong></h6>
+      <p>by {props.brand}</p>
+      <ul>
+        <li>{props.price}</li>
+        <li>{props.abv} ABV</li>
+      </ul>
     </div>
   );
 }
+
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.string,
+  abv: PropTypes.string,
+  pints: PropTypes.string
+};
 
 export default Keg;
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import Keg from './Keg';
+
+const availableKegs = [
+  {
+    name: 'Manny\'s Pale Ale',
+    brand: 'Georgetown Brewing Co.',
+    price: '$6.00',
+    abv: '5.4%',
+    pints: '124'
+  },
+  {
+    name: 'Pyramid Hefeweizen',
+    brand: 'Pyramid Brewing Co.',
+    price: '$5.00',
+    abv: '5.2%',
+    pints: '124'
+  },
+  {
+    name: 'Red Hook ESB',
+    brand: 'Red Hook Ale Brewery',
+    price: '$5.00',
+    abv: '5.8%',
+    pints: '124'
+  },
+  {
+    name: 'Interurban IPA',
+    brand: 'Fremont Brewing',
+    price: '$6.50',
+    abv: '6.2%',
+    pints: '124'
+  },
+  {
+    name: 'Lucille IPA',
+    brand: 'Georgetown Brewing Co.',
+    price: '$6.50',
+    abv: '7.0%',
+    pints: '124'
+  }
+];
 
 function KegList(){
 
   return (
     <div>
-      <p>Keg List</p>
+      {availableKegs.map((keg, index) =>
+        <Keg
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          abv={keg.abv}
+          pints={keg.pints}
+          key={index} />
+      )}
     </div>
   );
 }
